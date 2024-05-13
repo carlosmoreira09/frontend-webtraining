9ee88a9f5da72c5e56aa0260c0b1acfc8c363e6a
Carlos Moreira <cesmoreira90@hotmail.com>
2024-05-13 22:07:26 +0100

    adjusts in modal sheet

--- src/app/components/admin/sheets/components/modal-sheet/modal-sheet.component.ts
+++ src/app/components/admin/sheets/components/modal-sheet/modal-sheet.component.ts
@@ -47,6 +47,8 @@ export class ModalSheetComponent implements  OnInit {
   exercises: ExerciseModel[]
   listExercise: Exercise[] = [];
   sheets: any[]  = [];
+  resultExercise: Exercise | undefined;
+  resultSheet: string;
   public addExercisesA: Exercise[] = [];
   public addExercisesB: Exercise[] = [];
   public addExercisesC: Exercise[] = [];
@@ -62,10 +64,7 @@ export class ModalSheetComponent implements  OnInit {
 
   ngOnInit() {
     this.initNewControlForm();
-    this.getField('sheet_id')?.setValue(this.sheets[0].name)
     this.getField('exercises')?.setValue(this.listExercise[0].name)
-    console.log(this.getField('exercises')?.value)
-    console.log(this.getField('sheet_id')?.value)
 
   }
   initModalities() {
@@ -80,22 +79,15 @@ export class ModalSheetComponent implements  OnInit {
     ];
     return this.modalidades[0].abbrev;
   }
-  initSheet() {
-         this.sheets  = [
-      {name: 'Treino A', id: 'sheet_a'},
-      {name: 'Treino B', id: 'sheet_b'},
-      {name: 'Treino C', id: 'sheet_c'},
-      {name: 'Treino D', id: 'sheet_d'},
-    ];
-    return this.sheets[0].name
-  }
   initNewControlForm() {
     this.sheetFormGroup = this.formBuilder.group({
       exercises: [ this.initExerciseType(), [Validators.required, Validators.minLength(5)]],
       sheet_desc: [''],
       sheet_name: ['', Validators.required],
       exercise_type: [this.initModalities()],
-      sheet_id: [this.initSheet()],
+      sheet_id: ['sheet_a'],
+      quantity: [1],
+      sheet_details: ['']
     });
   }
    initExerciseType() {
@@ -110,9 +102,9 @@ export class ModalSheetComponent implements  OnInit {
       });
      return this.listExercise[0].name;
   }
+
   onSelectExercise() {
     const type = this.getField('exercise_type')?.value;
-    console.log(type)
     this.exerciseService.listExerciseByType(type).subscribe(
       (users: ExerciseModel[]) => {
         this.listExercise = [];
@@ -121,30 +113,62 @@ export class ModalSheetComponent implements  OnInit {
           const exercicio: Exercise = {name: k.exercise, id: k.id_exercise};
           this.listExercise.push(exercicio);
         }
+        this.resultExercise = this.listExercise[0];
+        this.resultSheet =  this.getField('sheet_id')?.value;
       });
+
     return this.listExercise;
   }
+  onSubmit() {
+    const sheet_name: string = this.getField('sheet_name')?.value;
+    const sheet_desc: string = this.getField('sheet_desc')?.value;
+    const sheet_detais: string = this.getField('sheet_detais')?.value;
+    const sheet_a: Exercise[] = this.addExercisesA;
+    const sheet_b: Exercise[] = this.addExercisesB;
+    const sheet_c: Exercise[] = this.addExercisesC;
+    const sheet_d: Exercise[] = this.addExercisesD;
+  }
    addExercise() {
-     const resultSheet: Sheet | undefined = this.sheets.find(({ name }) => name === this.getField('sheet_id')?.value);
-     const resultExercise: Exercise | undefined = this.listExercise.find(({ name }) => name === this.getField('exercises')?.value);
-     if (resultExercise && resultSheet) {
-       if (resultSheet.id === 'sheet_a') {
-         this.addExercisesA.push(resultExercise)
+     this.resultSheet =  this.getField('sheet_id')?.value;
+      this.resultExercise = this.listExercise.find(({ name }) => name === this.getField('exercises')?.value);
+     console.log(this.resultExercise, this.resultSheet)
 
-       } else if (resultSheet.id === 'sheet_b') {
-         this.addExercisesB.push(resultExercise)
+     if (this.resultExercise) {
+       if (this.resultSheet === 'sheet_a') {
+         this.addExercisesA.push(this.resultExercise)
 
-       } else if (resultSheet.id === 'sheet_c') {
-         this.addExercisesC.push(resultExercise)
+       } else if (this.resultSheet === 'sheet_b') {
+         this.addExercisesB.push(this.resultExercise)
 
-       } else if (resultSheet.id === 'sheet_d') {
-         this.addExercisesD.push(resultExercise)
+       } else if (this.resultSheet === 'sheet_c') {
+         this.addExercisesC.push(this.resultExercise)
+
+       } else if (this.resultSheet === 'sheet_d') {
+         this.addExercisesD.push(this.resultExercise)
        }
      } else {
        alert('error')
      }
    }
+  removeExercise(exercise: Exercise) {
+    const resultSheet =  this.getField('sheet_id')?.value;
+    if (exercise) {
+      if (resultSheet === 'sheet_a') {
+        this.addExercisesA.splice(this.addExercisesA.indexOf(exercise), 1);
 
+      } else if (resultSheet === 'sheet_b') {
+        this.addExercisesB.splice(this.addExercisesB.indexOf(exercise), 1);
+
+      } else if (resultSheet === 'sheet_c') {
+        this.addExercisesC.splice(this.addExercisesC.indexOf(exercise), 1);
+
+      } else if (resultSheet === 'sheet_d') {
+        this.addExercisesD.splice(this.addExercisesD.indexOf(exercise), 1);
+      }
+    } else {
+      alert('error')
+    }
+  }
   openDialogCreate() {
     this.initNewControlForm();
     this.showCreateSheet = !this.showCreateSheet;
