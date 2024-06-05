673c6779c4df3b66ee1a000b39162badac985c51
Carlos Moreira <cesmoreira90@hotmail.com>
2024-06-05 13:23:58 +0100

    add card to access register inside the home

--- src/app/components/admin/home/home.component.ts
+++ src/app/components/admin/home/home.component.ts
@@ -1,11 +1,13 @@
 import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {CardHomeComponent} from "./components/card-home.component";
 import { RouterLink} from "@angular/router";
-import {isPlatformBrowser} from "@angular/common";
+import {isPlatformBrowser, NgClass} from "@angular/common";
 import {initFlowbite} from "flowbite";
 import {StorageService} from "../../../service/storage.service";
 import {MessageService} from "primeng/api";
 import {ToastModule} from "primeng/toast";
+import {AuthRoles} from "../../../models/auth.model";
+import {jwtDecode} from "jwt-decode";
 
 @Component({
   selector: 'app-home',
@@ -13,19 +15,25 @@ import {ToastModule} from "primeng/toast";
   imports: [
     CardHomeComponent,
     RouterLink,
-    ToastModule
+    ToastModule,
+    NgClass
   ],
   providers: [MessageService,StorageService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+  isAdmin: boolean = false;
+  constructor(@Inject(PLATFORM_ID) private platformId: Object,
+              private storageService: StorageService,) {
 
   }
   ngOnInit() {
     if(isPlatformBrowser(this.platformId)){
       initFlowbite();
     }
+    const token = this.storageService.getUser();
+    const authRoles: AuthRoles = jwtDecode(token);
+    authRoles.role === 'admin'? this.isAdmin = true : this.isAdmin = false;
   }
 }
