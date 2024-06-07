cf32116c78dc901e3c37f3d2310b13d7e66f2e33
Carlos Moreira <cesmoreira90@hotmail.com>
2024-06-08 00:09:04 +0100

    refactor:  filter sheets  by user

--- src/app/service/sheets.service.ts
+++ src/app/service/sheets.service.ts
@@ -3,6 +3,9 @@ import {HttpClient} from "@angular/common/http";
 import {createNewSheet, SheetsModel} from "../models/sheets.model";
 import {ReturnMessage} from "../models/exercise.model";
 import {Observable} from "rxjs";
+import {AuthRoles} from "../models/auth.model";
+import {jwtDecode} from "jwt-decode";
+import {StorageService} from "./storage.service";
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +13,13 @@ import {Observable} from "rxjs";
 export class SheetsService {
   private url: string = "http://localhost:3000/api/sheets";
 
-  constructor(@Self() private httpClient: HttpClient) { }
+  constructor(@Self() private httpClient: HttpClient,
+              private storageService: StorageService,) { }
 
   listSheets() {
-    return this.httpClient.get<SheetsModel[]>(this.url)
+    const token = this.storageService.getUser();
+    const authRoles: AuthRoles = jwtDecode(token);
+    return this.httpClient.get<SheetsModel[]>(this.url+"/client/"+authRoles.id);
   }
 
   addNewSheet(newSheet: createNewSheet): Observable<any> {
