d34fb8685496c130174e23410bacc3514624ff53
Carlos Moreira <cesmoreira90@hotmail.com>
2024-08-14 14:21:22 +0000

    style: remove hover, add rounterlinkactive in menu

--- src/app/components/shared/menu/menu.component.ts
+++ src/app/components/shared/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
 import {CommonModule, isPlatformBrowser} from "@angular/common";
-import {RouterLink, RouterLinkActive} from "@angular/router";
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 import {initFlowbite} from "flowbite";
 import {StorageService} from "../../../service/storage/storage.service";
 
@@ -21,6 +21,7 @@ export class MenuComponent implements OnInit {
   isUser: boolean = false;
   isClient: boolean = false;
   constructor(@Inject(PLATFORM_ID) private platformId: Object,
+              private router: Router,
               private storageService: StorageService) {
   }
 
@@ -31,10 +32,10 @@ export class MenuComponent implements OnInit {
     const token = this.storageService.getItem('user');
     const tokenLocal = this.storageService.getItemLocalStorage('user');
     let tokenExist = tokenLocal ? tokenLocal : token
-    if(tokenExist.userType == 'admin') {
+    if(tokenExist.userType === 'admin') {
       return true;
     }
-
     return tokenExist.userType === 'user'? this.isUser = true : this.isClient = true;
     }
+
 }
